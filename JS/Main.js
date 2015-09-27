72b7aaf016844e0689cd55ab90881da5c4fc0c68
RedPanduzer <r.k.n.post@gmail.com>
2015-09-27 22:19:46 +0300

    Korjattu pause

--- JS/Main.js
+++ JS/Main.js
@@ -1,4 +1,4 @@
 
 var game = new Phaser.Game(1280, 800, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-game.stop();
+game.paused = true;
 var menu = new Phaser.Game(1280, 800, Phaser.AUTO, '', { preload: menu_preload, create: menu_create, update: menu_update });

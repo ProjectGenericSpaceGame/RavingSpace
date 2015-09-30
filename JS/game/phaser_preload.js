02f76b2569cc630f505e31ec3c38c37931498809
RedPanduzer <r.k.n.post@gmail.com>
2015-09-30 20:47:49 +0300

    Fixed imports

--- JS/game/phaser_preload.js
+++ JS/game/phaser_preload.js
@@ -3,9 +3,9 @@ function preload(){
 	game.world.setBounds(0, 0, 1600, 1000);
 	//Ladataan tausta ja asteroidit
     game.load.image('background', 'assets/sprites/VS_background_purple.png');
-    game.load.image('asteroid1', 'assets/sprites/VS_peli_asteroidi4.png');
-    game.load.image('asteroid2', 'assets/sprites/VS_peli_asteroidi2.png');
-    game.load.image('asteroid3', 'assets/sprites/VS_peli_asteroidi3.png');
+    game.load.image('asteroid1', 'assets/sprites/VS_peli_asteroid1.png');
+    game.load.image('asteroid2', 'assets/sprites/VS_peli_asteroid2.png');
+    game.load.image('asteroid3', 'assets/sprites/VS_peli_asteroid3.png');
 	//Ladataan alus
-	game.load.image('ship', 'assets/sprites/PlayerShip.png');
+	game.load.image('ship', 'assets/sprites/VS_peli_ship.png');
 }
